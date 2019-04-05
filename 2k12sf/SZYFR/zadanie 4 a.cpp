2181139834db5d828fa81778dd866e79af06d277
Krzysztof Wydrzynski <krzysztof.wydrzynski99@gmail.com>
2019-04-05 17:17:12 +0200

    matura 2k12sf

---
+++ 2k12sf/SZYFR/zadanie 4 a.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {	
+	ifstream plik("tj.txt");
+	ifstream klucze("klucze1.txt");
+	ofstream off("wynik4a.txt");
+	
+	if (!plik || !klucze) {
+		cout << "Blad!";
+		return 0;
+	}
+	
+	vector<string> kluczee;
+	
+	string a;
+	
+	while (!klucze.eof()) {
+		klucze >> a;
+		kluczee.push_back(a);
+	}
+	
+
+	int j, nr = 0;
+	string tekst, klucz;
+	
+	while(!plik.eof()) {
+		j = 0;
+		plik >> tekst;
+		klucz = kluczee[nr];
+		int lent = tekst.length();
+		int lenk = klucz.length();
+	 	for(int i = 0; i < lent; i++) {
+			if (j >= lenk) j = 0;
+			char a = (int)tekst[i] + ((int)klucz[j] - 64);
+			while((int)a > 90) {
+				a = (int)a - 26;
+			}
+			off << a;
+			j++;
+		}
+		off << endl;
+		nr++;
+	}
+
+	return 0;
+}
